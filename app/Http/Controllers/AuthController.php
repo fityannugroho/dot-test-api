6a8a2a8ff583efeb6f0ded4f8a0da4161724b4ea
Fityan <fityannugroho@gmail.com>
2022-09-11 21:15:35 +0700

    Update `login()` responses

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,11 +3,14 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\LoginRequest;
+use App\Traits\ApiResponser;
 use Illuminate\Support\Carbon;
 use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
 {
+    use ApiResponser;
+
     public function login(LoginRequest $request)
     {
         $validated = $request->validated();
@@ -17,23 +20,16 @@ class AuthController extends Controller
         ];
 
         if (!Auth::attempt($credentials)) {
-            return response()->json([
-                'statusCode' => 401,
-                'message' => 'Unauthorized'
-            ], 401);
+            return $this->error('Unauthorized', 401);
         }
 
         $user = $request->user();
         $token = $user->createToken('authToken')->plainTextToken;
 
-        return response()->json([
-            'statusCode' => 200,
-            'message' => 'success',
-            'data' => [
-                'access_token' => $token,
-                'token_type' => 'Bearer',
-                'expires_at' => Carbon::now()->addMinutes(config('sanctum.expiration'))->toDateTimeString()
-            ]
-        ], 200);
+        return $this->success([
+            'access_token' => $token,
+            'token_type' => 'Bearer',
+            'expires_at' => Carbon::now()->addMinutes(config('sanctum.expiration'))->toDateTimeString()
+        ]);
     }
 }
