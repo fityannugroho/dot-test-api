f55d369432ca17c6611bc3ed24fc03d8e85e5408
Fityan <fityannugroho@gmail.com>
2022-09-11 21:11:26 +0700

    Change `fetch()` helper params

--- app/Models/City.php
+++ app/Models/City.php
@@ -94,15 +94,14 @@ class City extends Model
     public static function getFromExternal(string $id = '')
     {
         $url = config('source.external_url') . '/city';
+        $options['query']['key'] = config('source.external_key');
+        $options['returnTarget'] = config('source.external_data_path');
 
-        return static::parseFromExternal(fetch(
-            $url,
-            config('source.external_key'),
-            config('source.external_data_path'),
-            [
-                'id' => $id
-            ]
-        ));
+        if (!empty($id)) {
+            $options['query']['id'] = $id;
+        }
+
+        return static::parseFromExternal(fetch($url, $options));
     }
 
     /**
